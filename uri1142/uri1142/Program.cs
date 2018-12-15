1e0b47d7c92b98a9c8545ecdc27dbab67a9b02e7
Nelio Alves <acenelio@gmail.com>
2018-12-15 09:55:05 -0200

    for update

---
+++ uri1142/uri1142/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uri1142 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int inicio = 1;
+
+            for (int i = 1; i <= n; i++) {
+
+                int segundo = inicio + 1;
+                int terceiro = inicio + 2;
+                Console.WriteLine(inicio + " " + segundo + " " + terceiro + " PUM");
+
+                inicio = inicio + 4;
+            }
+        }
+    }
+}
