5456089f36256cf9f761e4a2ed7c4f36e154008c
Nelio Alves <acenelio@gmail.com>
2019-03-25 23:54:27 -0300

    URI 1066

---
+++ uri1066/uri1066/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Curso {
+    class Program {
+        static void Main(string[] args) {
+            int num, pares, impares, positivos, negativos, i;
+
+            pares = 0;
+            impares = 0;
+            positivos = 0;
+            negativos = 0;
+
+            for (i = 1; i <= 5; i++) {
+                num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0) {
+                    pares++;
+
+                }
+                else {
+                    impares++;
+                }
+                if (num > 0) {
+                    positivos++;
+                }
+                else if (num < 0) {
+                    negativos++;
+                }
+            }
+            Console.WriteLine(pares + " valor(es) par(es)");
+            Console.WriteLine(impares + " valor(es) impar(es)");
+            Console.WriteLine(positivos + " valor(es) positivo(s)");
+            Console.WriteLine(negativos + " valor(es) negativo(s)");
+
+            Console.ReadLine();
+
+        }
+    }
+}
