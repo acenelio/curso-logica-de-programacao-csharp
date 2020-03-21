94a62d35c14e98d4d7d35f559318470791cc3e6d
Nelio Alves <acenelio@gmail.com>
2020-03-21 10:36:43 -0300

    URI 1096

---
+++ uri1096/uri1096/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class URI {
+    static void Main(string[] args)
+    {
+
+        for (int i = 1; i <= 9; i += 2) {
+            for (int j = 7; j >= 5; j--) {
+                Console.Write("I=" + i + " J=" + j + "\n");
+            }
+        }
+    }
+}
