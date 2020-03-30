17234ed3700c101ea25a2fb3a372aa6756938f9f
Nelio Alves <acenelio@gmail.com>
2020-03-30 09:35:07 -0300

    URI 1149

---
+++ uri1149/uri1149/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace uri1149 {
+    class Program {
+        static void Main(string[] args)
+        {
+            int A, N, i, soma;
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            A = int.Parse(valores[0]);
+
+            i = 1;
+            N = int.Parse(valores[i]);
+            while (N <= 0) {
+                i = i + 1;
+                N = int.Parse(valores[i]);
+            }
+
+            soma = 0;
+            for (i = 0; i < N; i++) {
+                soma = soma + A + i;
+            }
+
+            Console.WriteLine(soma);
+
+        }
+    }
+}
