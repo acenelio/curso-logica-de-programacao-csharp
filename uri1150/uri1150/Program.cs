fbf264650848fb2d584174b8ee49d251e767d360
Nelio Alves <acenelio@gmail.com>
2020-04-13 23:40:45 -0300

    URI 1150

---
+++ uri1150/uri1150/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uri1150 {
+    class Program {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+
+            while (z <= x) {
+                z = int.Parse(Console.ReadLine());
+            }
+
+            int cont = 1;
+            int soma = x;
+            do {
+                x = x + 1;
+                soma = soma + x;
+                cont = cont + 1;
+            } while (soma <= z);
+
+            Console.WriteLine(cont);
+        }
+    }
+}
