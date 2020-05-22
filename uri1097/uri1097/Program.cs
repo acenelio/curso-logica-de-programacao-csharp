9d05be1997a3e7c9df316914a8c17ba935e47ec4
Nelio Alves <acenelio@gmail.com>
2020-05-22 11:16:48 -0300

    URI 1097

---
+++ uri1097/uri1097/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uri1097 {
+    class Program {
+        static void Main(string[] args) {
+
+            int j = 7;
+            for (int i = 1; i <= 9; i = i + 2) {
+                Console.WriteLine("I=" + i + " J=" + j);
+                Console.WriteLine("I=" + i + " J=" + (j - 1));
+                Console.WriteLine("I=" + i + " J=" + (j - 2));
+                j = j + 2;
+            }
+        }
+    }
+}
