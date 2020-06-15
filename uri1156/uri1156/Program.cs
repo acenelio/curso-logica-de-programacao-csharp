d000497ff82bfd915cf34462d8caeb42994a1041
Nelio Alves <acenelio@gmail.com>
2020-06-15 18:52:23 -0300

    URI 1156

---
+++ uri1156/uri1156/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using System.Globalization;
+
+class uri1156 {
+    static void Main(string[] args) {
+
+        int num;
+        double den = 1.0, s = 0.0;
+        for (num = 1; num <= 39; num += 2) {
+            s += num / den;
+            den *= 2.0;
+        }
+        Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+
+        Console.ReadLine();
+    }
+}
