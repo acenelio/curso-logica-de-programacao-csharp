bf80772fbff4d293f1fb3bc97c57bc8567abb8c1
Nelio Alves <acenelio@gmail.com>
2020-06-26 23:21:56 -0300

    URI 1133

---
+++ uri1133/uri1133/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri1133 {
+    class Program {
+        static void Main(string[] args) {
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > y) {
+                int aux = x;
+                x = y;
+                y = aux;
+            }
+
+            for (int i = x+1; i < y; i++) {
+                if (i % 5 == 2 || i % 5 == 3) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
