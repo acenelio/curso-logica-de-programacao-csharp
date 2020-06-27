86c5a71dadfb727b7f74b38d3bf1fd1e632a2972
Nelio Alves <acenelio@gmail.com>
2020-06-26 23:33:38 -0300

    URI 1165

---
+++ uri1165/uri1165/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace uri1165 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++) {
+
+                int x = int.Parse(Console.ReadLine());
+                int metade = x / 2;
+
+                bool primo = true;
+
+                for (int j = 2; j <= metade; j++) {
+                    if (x % j == 0) {
+                        primo = false;
+                        break;
+                    }
+                }
+
+                if (primo) {
+                    Console.WriteLine(x + " eh primo");
+                }
+                else {
+                    Console.WriteLine(x + " nao eh primo");
+                }
+            }
+        }
+    }
+}
