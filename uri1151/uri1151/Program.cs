fc25c6cab88f6255d6f8e866708b40334ca4d954
Nelio Alves <acenelio@gmail.com>
2020-07-14 08:26:45 -0300

    URI 1151

---
+++ uri1151/uri1151/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uri1151 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i = i + 1) {
+                if (i == n - 1) {
+                    Console.WriteLine(a);
+                }
+                else {
+                    Console.Write(a + " ");
+                }
+                int aux = b;
+                b = a;
+                a = aux + b;
+            }
+        }
+    }
+}
