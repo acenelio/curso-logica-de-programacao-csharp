794fb8b49ae51652921372640cc50e11fe322fa8
Nelio Alves <acenelio@gmail.com>
2020-07-24 11:53:38 -0300

    URI 1155

---
+++ uri1155/uri1155/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1155 {
+    class Program {
+        static void Main(string[] args) {
+
+            double soma = 0;
+            for (int i = 1; i <= 100; i++) {
+                soma = soma + 1.0 / i;
+            }
+
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
