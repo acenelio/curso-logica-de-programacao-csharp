c9bfa60f5790d16a8e3751cd053c150f6dff44c7
Nelio Alves <acenelio@gmail.com>
2020-08-10 07:41:24 -0300

    uri 1095

---
+++ uri1095/uri1095/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace uri1095 {
+    class Program {
+        static void Main(string[] args) {
+
+            int i = 1;
+            for (int j = 60; j >= 0; j = j - 5) {
+                Console.WriteLine("I=" + i + " J=" + j);
+                i = i + 3;
+            }
+        }
+    }
+}
