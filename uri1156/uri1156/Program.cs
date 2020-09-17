0bbba7b7cddd159eec88544118cf808b22a7a717
acenelio <acenelio@gmail.com>
2020-09-17 09:54:38 -0300

    Update Program.cs

--- uri1156/uri1156/Program.cs
+++ uri1156/uri1156/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 using System.Globalization;
 
@@ -6,12 +6,12 @@ class uri1156 {
     static void Main(string[] args) {
 
         int num;
-        double den = 1.0, s = 0.0;
+        double den = 1.0, soma = 0.0;
         for (num = 1; num <= 39; num += 2) {
-            s += num / den;
-            den *= 2.0;
+            soma = soma + num / den;
+            den = den * 2.0;
         }
-        Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
 
         Console.ReadLine();
     }
